fdddc65175a173e7b440d1342eb4fe6cf4471d1b
LhaisCosta99 <lhaisferreirada@gmail.com>
2023-01-18 21:35:42 -0300

    feat: adição de imagens

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,12 @@ import yoda from "../../assets/babyYoda.png"
 export default function Header(){
     return(
         <HeaderStyle>
-            <img src={yoda} alt="baby Yoda" />
-            <h1>Header</h1>
+            <div>
+                <img src={yoda} alt="baby Yoda" />
+            </div>
+            <div>
+                <h1>Padawan <br></br> App</h1>
+            </div>
         </HeaderStyle>
     )
 }
--- src/components/Header/Style.js
+++ src/components/Header/Style.js
@@ -5,7 +5,20 @@ export const HeaderStyle = styled.header`
     display: flex;
     width: 100%;
     height: 100px;
+    align-items: center;
+    justify-content: flex-start;
+    div{
+        display: flex;
+        width: 40%;
+        justify-content: flex-start;
+    }
     h1{
         color: #ffc340;
+        font-family: 'Star Wars', sans-serif;
+        text-align: center;
+    }
+    img{
+        margin-left: 20px;
+        width: 100px;
     }
 `
